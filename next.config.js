219c8b7e02ef89bf6144e5ce9213a9d2b8df19db
jhosep98 <jhosep149@hotmail.com>
2022-08-23 22:05:24 -0300

    feat: src baseUrl

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: [],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
